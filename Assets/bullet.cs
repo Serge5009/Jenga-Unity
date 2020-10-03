48aa131e716e4c04896ddf62167bfd2dbe7ccb23
Serge5009 <71557129+Serge5009@users.noreply.github.com>
2020-10-03 03:39:56 +0300

    First upload

---
+++ Assets/bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    float m_Speed = 25.0f;
+    Rigidbody m_Bullet;
+    public Vector3 direction;
+   // public Transformer Bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = new Vector3(-1f, 0f, 0f);
+        m_Bullet = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Bullet(BindingDirection * Time.deltaTime);
+        m_Bullet.velocity = direction * m_Speed;
+    }
+}
