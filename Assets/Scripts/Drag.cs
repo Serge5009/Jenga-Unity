cf9a0f27cd1a1aa51b359624507a2d37ffd5c335
Serge5009 <71557129+Serge5009@users.noreply.github.com>
2020-10-05 18:34:38 +0300

    It works!
    It ia a game! Not really good game but game!

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private Vector3 mOffset;
+
+    private float mZCoord;
+
+    void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+     //   Store offset = gameobject world pos - OnMouseDown worlf pos
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        //   pixel coordinates (x,y)
+        Vector3 mousePoint = Input.mousePosition;
+
+        // z coordinate of game object on screen
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    void OnMouseDrag() 
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    
+    }
+}
